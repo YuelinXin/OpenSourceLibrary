5c1cd4d09e766a7855ef853a4d184cb55d8e6ea3
YuelinXin <sc20yx2@leeds.ac.uk>
2021-12-26 02:22:34 +0800

    maintenance

--- OSL_Ultra/JS/basic_function.js
+++ OSL_Ultra/JS/basic_function.js
@@ -172,12 +172,14 @@ function lock(obj)
     if (click_lock % 2 != 0)
     {
         document.body.style.overflow="hidden";
+        obj.innerHTML="<path d='M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z'/>";
         obj.style.color="#871414";
         alert("Page Scroll Locked");
     }
     else
     {
         document.body.style.overflow="visible";
+        obj.innerHTML="<path d='M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2z'/>";
         obj.style.color="black";
         alert("Page Scroll Unlocked");
     }
